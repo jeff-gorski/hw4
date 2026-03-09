879649abf5ba18fd1185eeb66af38f0912d47fd4
jeff-gorski <jeff.gorski@kellogg.northwestern.edu>
2026-03-09 02:48:03 +0000

    base done and ready for fine tuning

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,13 +4,19 @@ class EntriesController < ApplicationController
   end
 
   def create
-    @entry = Entry.new
-    @entry["title"] = params["title"]
-    @entry["description"] = params["description"]
-    @entry["occurred_on"] = params["occurred_on"]
-    @entry["place_id"] = params["place_id"]
-    @entry.save
-    redirect_to "/places/#{@entry["place_id"]}"
+    @user = User.find_by({ "id" => session["user_id"] })
+    if @user != nil
+      @entry = Entry.new
+      @entry["title"] = params["title"]
+      @entry["description"] = params["description"]
+      @entry["occurred_on"] = params["occurred_on"]
+      @entry["place_id"] = params["place_id"]
+      @entry["user_id"] = session["user_id"]
+      @entry.save
+    else
+      flash["notice"] = "You must be logged in."
+    end
+    redirect_to "/places/#{params["place_id"]}"
   end
 
 end
