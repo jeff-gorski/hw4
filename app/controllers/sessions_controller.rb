55eaa3f7726d6f25a5e5d77523948151b66f49be
jeff-gorski <jeff.gorski@kellogg.northwestern.edu>
2026-03-09 02:56:54 +0000

    minor adjustments to login & logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,18 +10,39 @@ class SessionsController < ApplicationController
         flash["notice"] = "Welcome, #{@user["username"]}."
         redirect_to "/places"
       else
-        flash["notice"] = "Nope."
+        jokes = [
+          "Wrong password! What did the password say to the hacker? You'll never get inside my head.",
+          "Wrong password! Why did the password go to therapy? It had too many issues to unlock.",
+          "Wrong password! What's a password's least favourite game? Guess who.",
+          "Wrong password! Why was the password so short? It had low self-esteem.",
+          "Wrong password! What did the password say to the username? Without you, I just don't log in."
+        ]
+        flash["notice"] = jokes.sample
         redirect_to "/login"
       end
     else
-      flash["notice"] = "Nope."
+      jokes = [
+        "No account found! Did you forget you existed? Even your account doesn't know who you are.",
+        "No account found! You're like a ghost — no trace of you in our system.",
+        "No account found! Are you sure you signed up? Your account is playing hide and seek... and winning.",
+        "No account found! Even Waldo has been found more times than your account.",
+        "No account found! Our database looked everywhere and came back empty-handed."
+      ]
+      flash["notice"] = jokes.sample
       redirect_to "/login"
     end
   end
 
   def destroy
     session["user_id"] = nil
-    flash["notice"] = "Goodbye."
+    jokes = [
+      "Goodbye! Why did the user log out? Because they couldn't handle any more cookies.",
+      "Goodbye! Logging out is like leaving a party — you always wonder if anything fun happens after you leave.",
+      "Goodbye! Why did the user log out? Their session had too much drama.",
+      "Goodbye! You've logged out. Don't worry, your data will miss you.",
+      "Goodbye! Logging out? Even your browser looks sad right now."
+    ]
+    flash["notice"] = jokes.sample
     redirect_to "/login"
   end
 end
